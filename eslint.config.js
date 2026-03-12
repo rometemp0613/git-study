49fa2df5fa2afd175825d518bc22696ce125deaa
rometemp0613 <rometemp0613@users.noreply.github.com>
2026-03-13 02:55:47 +0900

    ci: ESLint + Prettier 린트/포매팅 자동 검사 추가

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      globals: globals.node
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "warn",
+      "eqeqeq": "error",
+    }
+  }
+];
---
+++ src/example.js
@@ -0,0 +1,11 @@
+const greeting = 'hello world';
+let unused = 42;
+
+function add(a, b) {
+  if (a == b) {
+    console.log("same!");
+  }
+  return a + b;
+}
+
+add(1, 2);
